97263d7fab2ddde3ef30a051bbbb381cf8878976
Vivek Shukla <vivekshuklahere@gmail.com>
2024-10-11 22:47:39 +0530

    feat: updated birth date and added now page

---
+++ app/(main)/now/page.tsx
@@ -0,0 +1,22 @@
+export default function NowPage() {
+  return (
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Now (Work in Progress)
+      </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>Welcome to my Now page!</p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          Current Focus (WIP)
+        </h2>
+        <p>wip</p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">
+          Learning & Growth (WIP)
+        </h2>
+        <p>wip</p>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import { BlogPosts } from "app/components/posts";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Vivek Shukla
+        vivek shukla
       </h1>
       <p className="mb-4">
-        {`I'm a 22-year-old Software Engineer working on some cool projects and always learning new things. My interests are diverse and include computer programming, machine learning, artificial intelligence, water desalination, renewable energy, physics, economics, finance, business, philosophy, and more. I'm always seeking out new challenges and opportunities to learn and grow.
+        {`i'm a 23-year-old software engineer working on some cool projects and always learning new things. my interests are diverse and include computer programming, machine learning, artificial intelligence, water desalination, renewable energy, physics, economics, finance, business, philosophy, and more. i'm always seeking out new challenges and opportunities to learn and grow.
 
 `}
       </p>
+      {/* <p className="mb-4">
+        check out my{" "}
+        <Link href="/now">
+          (<span className="font-medium">now page</span>)
+        </Link>{" "}
+        to see what i'm currently focused on.
+      </p> */}
+
       <div className="my-8">
+        <h2 className="font-medium text-xl mb-4 tracking-tighter">blog</h2>
         <BlogPosts />
       </div>
     </section>
